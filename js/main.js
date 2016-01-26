b3527663abb99fdb2e27146002c4ba951c4ed55f
Travis Axton <travis.axton@gmail.com>
2016-01-26 11:09:27 -0500

    Added Directional Map

--- js/main.js
+++ js/main.js
@@ -5,7 +5,43 @@ function displayMap() {
     document.getElementById('events').style.display="block";
     initMap();
 }
+    
     function initMap() {
+      var directionsService = new google.maps.DirectionsService;
+      var directionsDisplay = new google.maps.DirectionsRenderer;
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: {lat: 33.8222, lng: -78.6811},
+        disableDefaultUI: true,
+        zoomControl: true
+      });
+      directionsDisplay.setMap(map);
+
+      var onChangeHandler = function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+      };
+      document.getElementById('start').addEventListener('change', onChangeHandler);
+      document.getElementById('end').addEventListener('change', onChangeHandler);
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+      directionsService.route({
+        origin: document.getElementById('start').value,
+        destination: document.getElementById('end').value,
+        travelMode: google.maps.TravelMode.DRIVING
+      }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
+
+
+/*
+
+function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 33.8222, lng: -78.6811},
         zoom: 10,
@@ -13,6 +49,20 @@ function displayMap() {
         zoomControl: true
       });  
     }
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 function hideMap() {
