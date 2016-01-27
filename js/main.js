53cce369dd07d042030bbc483a786efd75525133
Travis Axton <travis.axton@gmail.com>
2016-01-27 11:28:54 -0500

    Experimenting with Google Map directions service

--- js/main.js
+++ js/main.js
@@ -5,7 +5,52 @@ function displayMap() {
     document.getElementById('events').style.display="block";
     initMap();
 }
-    
+
+    function initMap() {
+      var directionsDisplay = new google.maps.DirectionsRenderer;
+      var directionsService = new google.maps.DirectionsService;
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: {lat: 33.82, lng: -78.68},
+        disableDefaultUI: true,
+        zoomControl: true  
+      });
+      directionsDisplay.setMap(map);
+      directionsDisplay.setPanel(document.getElementById('right-panel'));
+
+      var control = document.getElementById('floating-panel');
+      control.style.display = 'block';
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(control);
+
+      var onChangeHandler = function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+      };
+      document.getElementById('start').addEventListener('change', onChangeHandler);
+      document.getElementById('end').addEventListener('change', onChangeHandler);
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+      var start = document.getElementById('start').value;
+      var end = document.getElementById('end').value;
+      directionsService.route({
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+      }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
+
+
+
+
+
+/*
+
     function initMap() {
       var directionsService = new google.maps.DirectionsService;
       var directionsDisplay = new google.maps.DirectionsRenderer;
@@ -37,9 +82,6 @@ function displayMap() {
         }
       });
     }
-
-
-/*
 
 function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
