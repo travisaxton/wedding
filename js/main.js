6e75edc92a6b4a3e5f644a3c191d9d4b3e38c9ca
Travis Axton <travis.axton@gmail.com>
2016-01-31 20:33:27 -0500

    Fixed welcome message and map is functional though far from perfect

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ function displayMap() {
 
 
     function initMap() {
+      $('#welcome').hide();    
       var directionsDisplay = new google.maps.DirectionsRenderer;
       var directionsService = new google.maps.DirectionsService;
       var map = new google.maps.Map(document.getElementById('map'), {
@@ -49,78 +50,15 @@ function displayMap() {
 
 
 
-/*
-
-    function initMap() {
-      var directionsService = new google.maps.DirectionsService;
-      var directionsDisplay = new google.maps.DirectionsRenderer;
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: {lat: 33.8222, lng: -78.6811},
-        disableDefaultUI: true,
-        zoomControl: true
-      });
-      directionsDisplay.setMap(map);
-
-      var onChangeHandler = function() {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-      };
-      document.getElementById('start').addEventListener('change', onChangeHandler);
-      document.getElementById('end').addEventListener('change', onChangeHandler);
-    }
-
-    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-      directionsService.route({
-        origin: document.getElementById('start').value,
-        destination: document.getElementById('end').value,
-        travelMode: google.maps.TravelMode.DRIVING
-      }, function(response, status) {
-        if (status === google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(response);
-        } else {
-          window.alert('Directions request failed due to ' + status);
-        }
-      });
-    }
-
-function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 33.8222, lng: -78.6811},
-        zoom: 10,
-        disableDefaultUI: true,
-        zoomControl: true
-      });  
-    }
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function hideMap() {
-    $('.closebtn').click(function() {
-        $('#events').hide();
-    });
-}
-*/
-
 $('.closebtn').click(function() {
     $('#events').hide();
+    $('#welcome').hide();
 });
 
 
 $('.closeEvent').click(function() {
     $('#events').hide();
+    $('#welcome').hide();
 });
 
 if($(window).width() > 800 ) {
